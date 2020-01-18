f739d85828d60b50f5e4b1414d722d06957cdf9e
Justin <justinkgibbons@gmail.com>
2020-01-18 02:11:34 -0800

    upgrade kotlin, add basic room classes, debug

--- app/src/main/java/com/chris/thomson/midlandsriders/Adapters/NotificationsViewAdapter.kt
+++ app/src/main/java/com/chris/thomson/midlandsriders/Adapters/NotificationsViewAdapter.kt
@@ -1,6 +1,7 @@
 package com.chris.thomson.midlandsriders.Adapters
 
 import android.app.Activity
+import android.app.Application
 import android.content.Context
 import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
@@ -9,12 +10,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+//import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.room.*
 import com.chris.thomson.midlandsriders.EventDetailActivity
 import com.chris.thomson.midlandsriders.R
 import com.chris.thomson.midlandsriders.ViewModels.Event
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.event_item.view.*
+//import kotlinx.coroutines.launch
 
 class NotificationsViewAdapter()
     : RecyclerView.Adapter<NotificationsViewAdapter.NotificationViewHolder>() {
@@ -44,4 +49,89 @@ class NotificationsViewAdapter()
     }
 }
 
-data class Word(val word: String) {}
+
+@Entity
+class Word(
+        @PrimaryKey @ColumnInfo(name = "word") val word: String)
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * from word ORDER BY word ASC")
+    fun getAlphabetizedWords(): LiveData<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word")
+    suspend fun deleteAll()
+}
+
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        WordRoomDatabase::class.java,
+                        "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
+
+class WordRepository(private val wordDao: WordDao) {
+
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
+
+/*
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    // The ViewModel maintains a reference to the repository to get data.
+    private val repository: WordRepository
+    // LiveData gives us updated words when they change.
+    val allWords: LiveData<List<Word>>
+
+    init {
+        // Gets reference to WordDao from WordRoomDatabase to construct
+        // the correct WordRepository.
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    /**
+     * The implementation of insert() in the database is completely hidden from the UI.
+     * Room ensures that you're not doing any long running operations on
+     * the main thread, blocking the UI, so we don't need to handle changing Dispatchers.
+     * ViewModels have a coroutine scope based on their lifecycle called
+     * viewModelScope which we can use here.
+     */
+
+    /*
+    fun insert(word: Word) = viewModelScope.launch {
+        repository.insert(word)
+    }*/
+}*/
