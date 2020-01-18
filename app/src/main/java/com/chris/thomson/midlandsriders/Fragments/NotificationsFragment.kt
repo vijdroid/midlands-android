6a1a032bbad4e58e356ac0a83d6965c70f3342ae
Justin <justinkgibbons@gmail.com>
2020-01-18 00:05:52 -0800

    add notifications layout xml

--- app/src/main/java/com/chris/thomson/midlandsriders/Fragments/NotificationsFragment.kt
+++ app/src/main/java/com/chris/thomson/midlandsriders/Fragments/NotificationsFragment.kt
@@ -44,7 +44,7 @@ class NotificationsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
